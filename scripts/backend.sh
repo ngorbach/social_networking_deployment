58c006eb8ffc78efa0c48859687babd8f1fbd214
Nico Gorbach <nico.gorbach@gmail.com>
2020-03-26 23:51:32 +0000

    Delete backend.sh

--- scripts/backend.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python manage.py makemigrations
-python manage.py migrate
-#python manage.py collectstatic --no-input
-python manage.py collectstatic
-gunicorn -w 4 -b 0.0.0.0:8000 Motion.wsgi:application

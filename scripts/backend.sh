cd5e8ed99b5fcc601bc3c461462d2f0d5a136674
Nico Gorbach <nico.gorbach@gmail.com>
2020-03-26 16:41:18 +0000

    Update run.sh

--- scripts/backend.sh
+++ scripts/backend.sh
@@ -3,5 +3,4 @@ python manage.py makemigrations
 python manage.py migrate
 #python manage.py collectstatic --no-input
 python manage.py collectstatic
-rm -rf /frontend/build/* && cp -r /frontend_tmp/* /frontend
 gunicorn -w 4 -b 0.0.0.0:8000 Motion.wsgi:application
